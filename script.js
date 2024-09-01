889dd42d0c237270be56914422d3fa9a6a65e38e
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2024-08-31 21:05:57 -0400

    replace projects with js

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const projParent = document.getElementById('projects');
 document.querySelectorAll('nav a').forEach(e => e.addEventListener('click', _ => change(e.dataset.id)));
 
-//Create Stoodle project
+// Stoodle project
 createNewProjectEntry(projParent, {
     title: 'Stoodle',
     description: 'A study tool plugin for students of schools using Moodle, an open-source Learning Management System.',
     thumbnail: 'images/thumbnails/stoodle.png',
     links: ['https://github.com/deandradejatwit/stoodle', 'none'],
     tags: ['php', 'sql', 'javascript', 'open-source']
+});
+
+// Lunar Shuttle Project
+createNewProjectEntry(projParent, {
+    title: 'Lunar Shuttle',
+    description: 'An action-rougelike game created in Godot.',
+    thumbnail: 'images/thumbnails/lunar-shuttle.png',
+    links: ['none', 'https://ballisticbear703.itch.io/lunar-shuttle'],
+    tags: ['godot', 'c-sharp']
+});
+
+// A Game To Die For Project
+createNewProjectEntry(projParent, {
+    title: 'A Game To Die For',
+    description: 'A card-based rock-paper-scissors game made in Unity.',
+    thumbnail: 'images/thumbnails/agtdf.png',
+    links: ['https://github.com/kongshijatwit/A-Game-To-Die-For', 'https://ballisticbear703.itch.io/a-game-to-die-for'],
+    tags: ['unity', 'c-sharp', 'game-jam']
+});
+
+// A Small Bowl Project
+createNewProjectEntry(projParent, {
+    title: 'A Small Bowl',
+    description: 'A game about fish and the meaning of life.',
+    thumbnail: 'images/thumbnails/a-small-bowl.png',
+    links: ['none', 'https://ballisticbear703.itch.io/a-small-bowl'],
+    tags: ['unity', 'c-sharp']
 });
 
 function change(n) {
@@ -17,56 +44,48 @@ function change(n) {
 }
 
 function createNewProjectEntry(parent, projectObj) {
-    // Create project entry div with classname 'js-project-entry'
+    // ---- Create project entry div ---- //
     const projectEntry = document.createElement('div');
     projectEntry.className = 'project-entry';
     
-    // Create div for the github/itch link (style="display: grid;)
+    
+    // ---- Create div for github/itch links ---- //
     const projectLinkContainer = document.createElement('div');
     projectLinkContainer.style = 'display: grid;';
 
-    // Create anchor for img
-        // Create img with class 'js-project-link' + either 'js-link-git' or 'js-link-itch'
-        // Append img to anchor
-    // Append anchor to grid div
+    // Add links
     if (projectObj.links.length !== 0) {
         projectObj.links.forEach(link => {
+            if (link === 'none' || link === '') return;
             const linkAnchor = document.createElement('a');
             if (link === projectObj.links[0]) linkAnchor.className = 'js-entry-link js-link-git';
             else linkAnchor.className = 'js-entry-link js-link-itch';
             linkAnchor.href = link;
+            linkAnchor.target = '_blank';
             projectLinkContainer.appendChild(linkAnchor);
         });
     }
-
-    // Append grid div to project-entry div
     projectEntry.appendChild(projectLinkContainer);
 
-    // -------------------------------------------- //
-    
-    // Create img for thumbnail (use imgsrc param here)
-        // Append img to project-entry
+
+    // ---- Create the thumbnail image ---- //
     const thumbnailImg = document.createElement('img');
     thumbnailImg.src = projectObj.thumbnail;
     projectEntry.appendChild(thumbnailImg);
 
-    // -------------------------------------------- //
 
-    // Create a div that contains all title, tag, and description (container)
-        // Create a div for the title and tags (entry-header: style="display: flex; align-items: center;")
-            // Create h1 for title
-            // Append h1 to entry-header div
+    // ---- Create the title, description, and tag ---- //
     const articleContainer = document.createElement('article');
     const articleHeader = document.createElement('div');
     articleHeader.style = 'display: flex; align-items: center;';
+
+    // Title
     const projectTitle = document.createElement('h1');
     projectTitle.className = 'js-project-title';
     projectTitle.innerText = projectObj.title;
     articleHeader.appendChild(projectTitle);
 
-    // Loop through tags array
-        // Create p for tags (begin each tag with '#') with class 'js-tags'
-        // Append p to entry-header div
+    // Tags
     if (projectObj.tags.length !== 0) {
         projectObj.tags.forEach(tag => {
             const tagElem = document.createElement('p');
@@ -75,18 +94,14 @@ function createNewProjectEntry(parent, projectObj) {
             articleHeader.appendChild(tagElem);
         });
     }
-
-    // Append entry-header div to container div
     articleContainer.appendChild(articleHeader);
 
-    // Create p for description
-    // Append p to container div
+    // Description
     const descElem = document.createElement('p').appendChild(document.createTextNode(projectObj.description));
     articleContainer.appendChild(descElem);
-    
-    // Append container div to project-entry div
+
+
+    // ---- Final Append ---- //
     projectEntry.appendChild(articleContainer);
-    
-    // Append project-entry div to parent
     parent.appendChild(projectEntry);
 }
